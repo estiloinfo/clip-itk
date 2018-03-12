981b5e06f0eb55ff33aee87bc5fe769ab5b2580a
Ricardo Brisighelli <soporte@estilo.com.ar>
2018-03-12 11:56:32 -0300

    clip-postgres: Fix save apostropho in char field . Add function ADDAPOST( value ) to replace ADDSLASH( value )

--- clip/_string.c
+++ clip/_string.c
@@ -2245,6 +2245,50 @@ clip_ADDSLASH(ClipMachine * mp)
 }
 
 int
+clip_ADDAPOST(ClipMachine * mp)
+{
+	int count=0,i,l;
+	unsigned char *ret;
+	unsigned char *str = (unsigned char *)_clip_parcl(mp, 1, &l);
+
+	if ( str==NULL || l==0 )
+	{
+		_clip_retc(mp,"");
+		return 0;
+	}
+	for (i=0; i<l; i++)
+	{
+		switch (str[i])
+		{
+			case '\'':
+			case 0   :
+				if(i==0 || str[i-1] != '\'')
+					count++;
+				break;
+		}
+	}
+	ret=malloc(l+count+1);
+	for (count=0,i=0; i<l; i++,count++)
+	{
+		switch (str[i])
+		{
+			case '\'':
+			case 0   :
+				if(i==0 || str[i-1] != '\'')
+				{
+					ret[count] = '\'';
+					count++;
+				}
+				break;
+		}
+		ret[count] = str[i]?str[i]:'0';
+	}
+	ret[count] = 0;
+	_clip_retcn_m(mp,(char *)ret,count);
+	return 0;
+}
+
+int
 clip_SPLITDELIM(ClipMachine * mp)
 {
 	ClipVar *ap = RETPTR(mp);
