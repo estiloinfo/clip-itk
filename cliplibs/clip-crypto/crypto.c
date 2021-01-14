f27e9c77acb16fc8d39a07089f435e07dba4c680
Ricardo Brisighelli <soporte@estilo.com.ar>
2021-01-14 11:49:05 -0300

    clip-crypto: Add BASE64ENCODE and BASE64DECODE functions             BASE64ENCODE( <cData> ) -> cDataEncode64             BASE64DECODE( <cDataEncode64> ) -> cData

--- cliplibs/clip-crypto/crypto.c
+++ cliplibs/clip-crypto/crypto.c
@@ -24,6 +24,7 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <math.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -31,6 +32,7 @@
 
 #include <openssl/evp.h>
 #include <openssl/sha.h>
+#include <openssl/bio.h>
 
 #include "clip.h"
 #include "error.ch"
@@ -299,4 +301,111 @@ int clip_SHA1_FILE(ClipMachine *mp)
 	return 0;
 
 }
+
+
+/*
+# Base64
+*/
+
+//Calculates the length of a decoded base64 string
+static int 
+calcDecodeLength(const char* b64input) 
+{ 
+	int len = strlen(b64input);
+	int padding = 0;
+
+  	if (b64input[len-1] == '=' && b64input[len-2] == '=') //last two chars are =
+    		padding = 2;
+	else if (b64input[len-1] == '=') //last char is =
+		padding = 1;
+
+	return (int)len*0.75 - padding;
+}
+
+//Encodes a string to base64
+static int 
+Base64Encode(char* message, char** buffer) 
+{
+	BIO *bio, *b64;
+	FILE* stream;
+  	int encodedSize = 4*ceil((double)strlen(message)/3);
+  	*buffer = (char *)malloc(encodedSize+1);
+
+	stream = fmemopen(*buffer, encodedSize+1, "w");
+	b64 = BIO_new(BIO_f_base64());
+	bio = BIO_new_fp(stream, BIO_NOCLOSE);
+	bio = BIO_push(b64, bio);
+	BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL); //Ignore newlines - write everything in one line
+	BIO_write(bio, message, strlen(message));
+	BIO_flush(bio);
+	BIO_free_all(bio);
+	fclose(stream);
+
+	return (0); //success
+}
+
+//Decodes a base64 encoded string
+static int 
+Base64Decode(char* b64message, char** buffer) 
+{ 
+	BIO *bio, *b64;
+	int decodeLen = calcDecodeLength(b64message),
+	len = 0;
+	*buffer = (char*)malloc(decodeLen+1);
+	FILE* stream = fmemopen(b64message, strlen(b64message), "r");
+
+	b64 = BIO_new(BIO_f_base64());  
+	bio = BIO_new_fp(stream, BIO_NOCLOSE);
+	bio = BIO_push(b64, bio);
+	BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL); //Do not use newlines to flush buffer
+	len = BIO_read(bio, *buffer, strlen(b64message));
+	//Can test here if len == decodeLen - if not, then return an error
+	(*buffer)[len] = '\0';
+
+	BIO_free_all(bio);
+	fclose(stream);
+
+	return (0); //success
+}
+
+int
+clip_BASE64DECODE(ClipMachine *mp)
+{
+	int len = 0;
+	char *str = _clip_parcl(mp, 1, &len );
+	char* base64DecodeOutput;
+
+        if (str == NULL)
+        {
+                _clip_retc(mp, "");
+                return _clip_trap_err(mp, EG_ARG, 0, 0, __FILE__, __LINE__, "BASE64DECODE");
+        }
+
+        Base64Decode(str, &base64DecodeOutput);
+
+        _clip_retc(mp, base64DecodeOutput );
+
+        return 0;
+}
+
+int
+clip_BASE64ENCODE(ClipMachine *mp)
+{
+	int len = 0;
+	char *str = _clip_parcl(mp, 1, &len );
+	char* base64EncodeOutput;
+
+        if (str == NULL)
+        {
+                _clip_retc(mp, "");
+                return _clip_trap_err(mp, EG_ARG, 0, 0, __FILE__, __LINE__, "BASE64ENCODE");
+        }
+
+        Base64Encode(str, &base64EncodeOutput);
+
+        _clip_retc(mp, base64EncodeOutput );
+
+        return 0;
+}
+
 
